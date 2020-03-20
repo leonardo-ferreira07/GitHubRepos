4964006ee358022652a43eff241aba4efa45a23c
Leonardo Kaminski Ferreira <leonardo.kaminski.ferreira@gmail.com>
2020-03-20 11:22:58 -0300

    Added PullRequestView

---
+++ SearchApp/Modules/Pull Requests/Views/PullRequestView.swift
@@ -0,0 +1,21 @@
+//
+//  PullRequestView.swift
+//  SearchApp
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 20/03/20.
+//  Copyright © 2020 Leonardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct PullRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PullRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        PullRequestView()
+    }
+}
